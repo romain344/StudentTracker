3816a0a1dcc1c1c80765883990d4674c8962613d
romain344 <romainleoulzec@gmail.com>
2026-03-12 09:08:29 +0100

    push for solve git problems

--- StudentTracker/src/main/java/com/studenttracker/model/Student.java
+++ StudentTracker/src/main/java/com/studenttracker/model/Student.java
@@ -5,20 +5,26 @@ public class Student {
     private Double grade;
 
     public Student(String name, Double grade) {
+        // this is a clase for name and garde
         this.name = name;
+        // for nota
         this.grade = grade;
+        // for grade
     }
 
     public String getName() {
+        // the clase for to recover the value for name
         return name;
     }
 
     public Double getGrade() {
+        // the clase for to recover the value for grade
         return grade;
     }
 
     @Override
     public String toString() {
+        // to recover name and grade
         return name + " - grade " + grade;
     }
 }
